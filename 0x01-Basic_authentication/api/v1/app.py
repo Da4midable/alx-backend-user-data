ed97be4a4c3790ed63b02282e2ced6455a3e3f87
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-03 23:11:00 +0100

    Task 5 - validation +

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+from typing import Optional
 import os
 
 app = Flask(__name__)
@@ -20,26 +21,31 @@ if auth_type:
         from api.v1.auth.auth import Auth
         auth = Auth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
     """ Not authorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def not_allowed(error) -> str:
     """ Not allowed handler """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.before_request
-def filter_request():
+def filter_request() -> Optional[None]:
     current_path = request.path
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
     if auth:
         if not auth.require_auth(current_path, excluded_paths):
             return
@@ -47,6 +53,7 @@ def filter_request():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
