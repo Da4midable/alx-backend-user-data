18e14837419fdd7923620f9717a1743f74604466
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-03 20:53:29 +0100

    paths that require authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """to validate authentication"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path + "/" in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """to authenticate header"""
