fa569e42f5531c30789f51c16003b73046bd5bf1
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-04 01:08:00 +0100

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """module creates a BasicAuth class"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -10,7 +11,7 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """
         Uses the Base64 part of the Authorization
-        header for a Basic Authentication
+        header for a basic authentication
         """
         if authorization_header is None:
             return None
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.strip("Basic ")
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """decodes value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
