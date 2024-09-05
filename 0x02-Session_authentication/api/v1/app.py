d6804022927551046a06c4fbd594420b24c1abfb
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-05 20:44:39 +0100

    123

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,15 +56,18 @@ def filter_request() -> Optional[None]:
     request.current_user = auth.current_user(request)
     current_path = request.path
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if auth:
         if not auth.require_auth(current_path, excluded_paths):
             return
         if auth.authorization_header(request) is None:
             abort(401)
+        if auth.session_cookie(request) is None:
+            abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        return None, abort(401)
 
 
 if __name__ == "__main__":
