0884277d34eb11b9f6da414d83c8671da12ceddb
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-05 18:02:58 +0100

    session ID created

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """module for creating a SessionAuth instance"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """creates a SessionAuth instance"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """method creates session ID"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        sess_id = str(uuid.uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
