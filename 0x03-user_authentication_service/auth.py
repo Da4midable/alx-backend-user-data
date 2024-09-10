1141e73322ba011df28ef6c09ee4e991844578d6
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-10 08:13:36 +0100

    0 through 5

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+defines a _hash_password method that takes in
+a password string arguments and returns bytes.
+"""
+
+import bcrypt
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    returns bytes of a salted hash of the input password,
+    hashed with bcrypt.hashpw
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,93 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database and returns the User object"""
+        session = self._session
+        if session is None:
+            raise ValueError("Session is None")
+        new_user = User(email=email, hashed_password=hashed_password)
+        session.add(new_user)
+        session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Finds a user by keyword arguments.
+
+        :param kwargs: Arbitrary keyword arguments to filter the user
+        :return: User object
+        :raises NoResultFound: If no user is found
+        :raises InvalidRequestError: If invalid query arguments are passed
+        """
+        session = self._session
+
+        try:
+            query = session.query(User).filter_by(**kwargs)
+            user = query.first()
+
+            if user is None:
+                raise NoResultFound()
+
+            return user
+
+        except NoResultFound:
+            raise NoResultFound()
+
+        except Exception:
+            raise InvalidRequestError()
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Updates a user's attributes based on the given keyword arguments.
+
+        :param user_id: the ID of the user to update
+        :param kwargs: attributes to update and their new values
+        :raises NoResultFound: if the user with the given ID is not found
+        :raises ValueError: if one of the given attributes does not exist
+        :return: None
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError
+
+            self._session.commit()
+
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError as e:
+            raise InvalidRequestError(e)
