3c86db74f3c32b87f9a5a5485bce0a81bfc5006e
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-09-12 13:09:16 +0100

    updates password and resets reset_token +

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,7 +98,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(user.id,
-                                 user.hashed_password=hashed_password,
-                                 user.reset_token=None)
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
